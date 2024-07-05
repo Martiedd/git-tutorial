8ca6ff416e6e033f9a82ceddc6c089814645aad7
Martiedd <6511130043@mut.ac.th>
2024-07-05 17:13:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php echo "my repository"?>
-<?php echo "3rd commit"?>
-
-<?php echo "Computer Engineering"?>
-<?php echo "MUT Naja"?>
-
-<?php echo "Making an edits"?>
+<?php echo "Martiedd"?>
 <?php echo "Making an edits"?>
